c56eed9f6688c4218c8aebda52e7aebfc96c193b
narindra2 <narindrandimby2@gmail>
2024-10-25 16:51:58 +0300

    gbug-recap-debug-7

--- js/main.js
+++ js/main.js
@@ -202,7 +202,6 @@
 				$('.number').each(function(){
 					var $this = $(this),
 						num = $this.data('number');
-						console.log(num);
 					$this.animateNumber(
 					  {
 					    number: num,
